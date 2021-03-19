0c54a01138dc41caee0b8bb8fc84b1ed8a847898
Thomas Proust <thomas.proust89@gmail.com>
2021-03-19 13:50:36 +0900

    remove xpath

--- SN_1.java
+++ SN_1.java
@@ -65,20 +65,20 @@ public class SQLInjection {
 ​
 ////////////////////////////////////////
 // XPath expressions should not be vulnerable to injection attacks
-​
-public class Xpath {
-  public boolean authenticate(HttpServletRequest request, XPath xpath, org.w3c.dom.Document doc) throws XPathExpressionException {
-    String user = request.getParameter("user");
-    String pass = request.getParameter("pass");
-​
-    String expression = "/users/user[@name='" + user + "' and @pass='" + pass + "']"; // Unsafe
-​
-    // An attacker can bypass authentication by setting user to this special value
-    user = "' or 1=1 or ''='";
-​
-    return (boolean)xpath.evaluate(expression, doc, XPathConstants.BOOLEAN); // Noncompliant
-  }
-}
+// ​
+// public class Xpath {
+//   public boolean authenticate(HttpServletRequest request, XPath xpath, org.w3c.dom.Document doc) throws XPathExpressionException {
+//     String user = request.getParameter("user");
+//     String pass = request.getParameter("pass");
+// ​
+//     String expression = "/users/user[@name='" + user + "' and @pass='" + pass + "']"; // Unsafe
+// ​
+//     // An attacker can bypass authentication by setting user to this special value
+//     user = "' or 1=1 or ''='";
+// ​
+//     return (boolean)xpath.evaluate(expression, doc, XPathConstants.BOOLEAN); // Noncompliant
+//   }
+// }
 ​
 ​
 ////////////////////////////////////////////
