546417ed3f51ead6b9a9cf1a7844bfade3c4ea07
Thomas Proust <thomas.proust89@gmail.com>
2021-04-05 00:40:19 +0900

    upate readme

--- SN_1.java
+++ SN_1.java
@@ -30,6 +30,7 @@ public class RequestProcessor {
   protected void processRequest(HttpServletRequest request) {
     ServletInputStream sis = request.getInputStream();
     ObjectInputStream ois = new ObjectInputStream(sis);
+    
     Object obj = ois.readObject(); // Noncompliant
   }
 }
