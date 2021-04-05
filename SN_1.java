34b9b1e7585040f54ca5bc6e659aee6a4e398721
Thomas Proust <thomas.proust89@gmail.com>
2021-04-05 18:03:57 +0900

    Update SN_1.java

--- SN_1.java
+++ SN_1.java
@@ -26,14 +26,14 @@ public class Redirection {
 ////////////////////////////////////
 // Deserialization should not be vulnerable to injection attacks
 ​
-public class RequestProcessor {
-  protected void processRequest(HttpServletRequest request) {
-    ServletInputStream sis = request.getInputStream();
-    ObjectInputStream ois = new ObjectInputStream(sis);
+// public class RequestProcessor {
+//   protected void processRequest(HttpServletRequest request) {
+//     ServletInputStream sis = request.getInputStream();
+//     ObjectInputStream ois = new ObjectInputStream(sis);
     
-    Object obj = ois.readObject(); // Noncompliant
-  }
-}
+//     Object obj = ois.readObject(); // Noncompliant
+//   }
+// }
 ​
 ​
 ​
