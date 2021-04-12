d317848a5297540ebd49c26c3e0214b4b90ef539
Thomas Proust <thomas.proust89@gmail.com>
2021-04-13 00:00:31 +0900

    brings back sql

--- SN_1.java
+++ SN_1.java
@@ -45,7 +45,7 @@ public class SQLInjection {
     String user = request.getParameter("user");
     String pass = request.getParameter("pass");
 ​
-    //String query = "SELECT * FROM users WHERE user = '" + user + "' AND pass = '" + pass + "'"; // Unsafe
+    String query = "SELECT * FROM users WHERE user = '" + user + "' AND pass = '" + pass + "'"; // Unsafe
 ​
 //     If the special value "foo' OR 1=1 --" is passed as either the user or pass, authentication is bypassed
 //     Indeed, if it is passed as a user, the query becomes:
